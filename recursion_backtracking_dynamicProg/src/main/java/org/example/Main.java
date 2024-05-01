824b5c5c7ac20a548a9bd50061c66fd19b32ece3
Juan Manuel Farizo <juanmanuelfarizo@gmail.com>
2024-04-30 21:33:41 -0300

    recursion

--- recursion_backtracking_dynamicProg/src/main/java/org/example/Main.java
+++ recursion_backtracking_dynamicProg/src/main/java/org/example/Main.java
@@ -1,7 +1,10 @@
 package org.example;
 
+import org.example.recursion.SumAlgorithm;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SumAlgorithm algorithm = new SumAlgorithm();
+        System.out.println(algorithm.recursion(5));
     }
 }
--- recursion_backtracking_dynamicProg/src/main/java/org/example/recursion/SumAlgorithm.java
+++ recursion_backtracking_dynamicProg/src/main/java/org/example/recursion/SumAlgorithm.java
@@ -1,2 +1,18 @@
-package org.example.recursion;public class SumAlgorithm {
+package org.example.recursion;
+
+public class SumAlgorithm {
+    public int iteration(int n) {
+        int result = 0;
+        for (int num = 0; num < n; num++) {
+            result += num;
+        }
+        return result;
+    }
+
+    public int recursion(int n) {
+        //Base case when to terminate the algorithm
+        if (n == 1) return 1;
+
+        return n + recursion(n-1);
+    }
 }
