8e138b386480b356f2c4a9ee0595d5af362736f1
Juan Manuel Farizo <jfarizo@k12.com>
2024-05-10 23:19:07 -0300

    delete file

--- recursion_backtracking_dynamicProg/src/main/java/org/example/recursion/ShoppingCart.java
+++
@@ -1,7 +0,0 @@
-package org.example.recursion;
-
-public sealed interface ShoppingCart {
-    record Initial() implements ShoppingCart{}
-    record Pending(String productItem) implements ShoppingCart{};
-    record Closed() implements ShoppingCart{};
-}
