7bf7c55758a7b1bf55791a365196cb8cc025cbc7
Juan Manuel Farizo <juanmanuelfarizo@gmail.com>
2024-05-14 09:32:42 -0300

    rust server

--- studying_rust/command_line_example/src/main.rs
+++ studying_rust/command_line_example/src/main.rs
@@ -1,21 +1,23 @@
 use std::io;
 
 fn main() {
+    print!("Enter your weight: ");
     let mut input = String::new(); // String is a type of smart pointer
 
-    io::stdin().read_line(&mut input);
+    io::stdin().read_line(&mut input).unwrap();
+    let weight: f32 = input.trim().parse().unwrap();
 
-    borrow_string(&input);
-    own_string(input);
+    // borrow_string(&input);
+    // own_string(input);
     // println!("input: {}", input);
-    // print!("Weight on Mars: {}kg", calculate_weight_on_mars(100.0));
+    print!("Weight on Mars: {}kg", calculate_weight_on_mars(weight));
 }
-fn borrow_string(s: &String) {
-    println!("{}", s);
-}
-fn own_string(s: String) {
-    println!("{}", s);
-}
+// fn borrow_string(s: &String) {
+//     println!("{}", s);
+// }
+// fn own_string(s: String) {
+//     println!("{}", s);
+// }
 
 fn calculate_weight_on_mars(weight: f32) -> f32 {
     (weight / 9.81) * 3.711 // Is the equivalent to: return ...;
