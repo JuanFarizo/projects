a594d96b61de1b6806a2dae26b5f93709f0d8e29
Juan Manuel Farizo <jfarizo@k12.com>
2024-05-21 16:46:49 -0300

    rust exer

--- studying_rust/hello_world/src/main.rs
+++ studying_rust/hello_world/src/main.rs
@@ -5,26 +5,121 @@
 use std::mem;
 //Entry point
 fn main() {
-    println!("result {}", exercise1());
+    exercise4();
+    //println!("result {}", exercise3());
+}
+/*
+A palindrome is a word, verse, or sentence that reads the same backward or forward,
+such as 'Able was I ere I saw Elba,' or a number like 1881.
+
+Write a function named is_palindrome() that checks whether a given string is a palindrome or not.
+The function should take a string as input and return a boolean value indicating whether the string is a palindrome or not.
+*/
+fn exercise4() {
+    let input = String::from("1211");
+    println!(
+        "It is {:?} that the given string is palindrome",
+        palindrome(input)
+    );
+}
+fn palindrome(input: String) -> bool {
+    input.
+    /* Your Code here */
+    false
 }
 
+/*
+This question involves writing code to analyze the production of an assembly line in a car factory.
+The assembly line has different speeds, ranging from 0 (off) to 10 (maximum).
+At the lowest speed of 1, the assembly line produces a total of 221 cars per hour.
+The production rate increases linearly with the speed,
+meaning that a speed of 4 produces 4 * 221 = 884 cars per hour.
+
+However, higher speeds increase the likelihood of producing faulty cars that need to be discarded.
+The success rate depends on the speed, as shown in the table below:
+· Speeds 1 to 4: 100% success rate.
+· Speeds 5 to 8: 90% success rate.
+· Speeds 9 and 10: 77% success rate.
+
+You need to write two functions:
+1. The first function, total_production(), calculates the total number of cars successfully produced without faults within a specified time given in hours. The function takes the number of hours and speed as input and returns the number of cars successfully produced.
+2. The second function, cars_produced_per_minute(), calculates the number of cars successfully produced per minute. The function takes the number of hours and speed as input and returns the number of cars produced per minute.
+Write the code for both functions based on the provided specifications.
+*/
+fn exercise3() {
+    println!("{}", total_production(6, 1)); // to round the values we use i32. just ignore for mow
+    println!("{}", cars_produced_per_minutes(6, 1))
+}
+fn total_production(hrs: u8, speed: u8) -> f32 {
+    let cars = match speed {
+        1..=4 => 221.0 * hrs as f32 * speed as f32,
+        5..=8 => 221.0 * hrs as f32 * speed as f32 * 0.9,
+        9..=10 => 221.0 * hrs as f32 * speed as f32 * 0.77,
+        _ => return 0.0,
+    };
+    println!("cars produced {}", cars);
+    cars
+}
+fn cars_produced_per_minutes(hours: u8, speed: u8) -> f32 {
+    let per_minute: f32;
+    /* Your code below this line*/
+    per_minute = total_production(hours, speed) / 60.0;
+    println!("cars produced per minute {}", per_minute);
+    per_minute
+}
+/*
+Write a program to find the sum of natural numbers below a given number N, where N is provided by the user.
+The sum should only include numbers that are multiples of either 3 or 5.
+For example, if the user enters N = 20, the multiples of 3 are 3, 6, 9, 12, 15, 18, and the multiples of 5 are 5, 10, and 15.
+
+Please note that the value of 15 will be considered only once since it is a multiple of both 3 and 5.
+The sum will be calculated as follows: 3 + 5 + 6 + 9 + 10 + 12 + 15 + 18.
+
+Write a program that takes the user input N, performs the necessary calculations, and outputs the sum.
+*/
+fn exercise2() -> i32 {
+    let mut n = String::new();
+    std::io::stdin()
+        .read_line(&mut n)
+        .expect("failed to read input.");
+    let n: i32 = n.trim().parse().expect("invalid input");
+    let mut result = 0;
+
+    for i in 1..=n {
+        if i % 3 == 0 || i % 5 == 0 {
+            result = result + i;
+            println!("sum the value {} to the result  {}", i, result)
+        }
+    }
+    result
+}
+
+/*
+Write a program to find the difference between the square of the sum and the sum of the squares of the first N numbers.
+N will be a user-defined input that your program will take.
+
+For example, if the user enters the number 5, you should compute the square of the sum as (1 + 2 + 3 + 4 + 5)^2 = 225.
+Next, compute the sum of the squares as (1^2 + 2^2 + 3^2 + 4^2 + 5^2) = (1 + 4 + 9 + 16 + 25) = 55.
+Finally, calculate the difference as 225 - 55 = 170.
+*/
 fn exercise1() -> i32 {
     let mut n = String::new();
     std::io::stdin()
         .read_line(&mut n)
         .expect("failed to read input.");
-    let mut n: i32 = n.trim().parse().expect("invalid input");
+    let n: i32 = n.trim().parse().expect("invalid input");
 
     let mut square_of_sum = 0;
     let mut sum_of_squares = 0;
 
-    while n <= 1 {
-        square_of_sum = square_of_sum + n;
-        sum_of_squares = sum_of_squares + (n * n);
-        n = n - 1;
+    for i in 1..=n {
+        square_of_sum = square_of_sum + i;
+        sum_of_squares = sum_of_squares + i.pow(2);
     }
-    println!("{}", (square_of_sum * square_of_sum) - sum_of_squares);
-    return (square_of_sum * square_of_sum) - sum_of_squares;
+
+    let result = (square_of_sum.pow(2)) - sum_of_squares;
+    println!("{}", result);
+    result
 }
 
 fn operators() {
