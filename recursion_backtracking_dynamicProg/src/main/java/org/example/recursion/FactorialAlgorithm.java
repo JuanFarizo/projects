22187b0cbcfa21ac873f550be42398cbe5126cff
Juan Farizo <juanmanuelfarizo@gmail.com>
2026-03-30 15:59:00 -0300

    fact algo

--- recursion_backtracking_dynamicProg/src/main/java/org/example/recursion/FactorialAlgorithm.java
+++ recursion_backtracking_dynamicProg/src/main/java/org/example/recursion/FactorialAlgorithm.java
@@ -1,7 +1,7 @@
 package org.example.recursion;
 
 public class FactorialAlgorithm {
-    //We have to return the values, otherwise we are not able to get
+    // We have to return the values, otherwise we are not able to get
     // the result because these sub results are depending extremely
     // heavily on each other, stack frames are relying on each other
 
@@ -19,4 +19,23 @@ public class FactorialAlgorithm {
             return acc;
         return tail(n - 1, n * acc);
     }
+
+    static int recursiveCollatzCalculator(int start, int step) {
+        // (x & 1) == 1); // true   ->  impar
+        // (x & 1) == 1); // false  ->  par
+        //Base Case 
+        if(start == 1) return step;
+        return recursiveCollatzCalculator((start & 1 ) == 1 ? (start * 3 + 1): start >> 1, ++step);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(recursiveCollatzCalculator(12, 0));
+    }
+
+
+    // (4, 1)
+    // Tail(4 - 1, 4 * 1 )
+    // Tail(3 - 1, 3 * 4 )
+    // Tail(2 - 1, 2 * 12 )
+    // n = 1 y return acc=24.
 }
